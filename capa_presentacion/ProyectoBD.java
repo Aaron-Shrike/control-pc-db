b6f13ca7f826eb3f91865a7e215a3abc2c0e46e2
Aarón Rojas <86794731+Aaron-Shrike@users.noreply.github.com>
2022-01-09 18:03:58 -0500

    archivos finales

---
+++ capa_presentacion/ProyectoBD.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capa_presentacion;
+
+/**
+ *
+ * @author ROGER
+ */
+public class ProyectoBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmMenuPrincipal  objMenu = new frmMenuPrincipal();
+        objMenu.setVisible(true);
+    }
+    
+}
